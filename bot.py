9f716b86260c3755b1dc513edf719884e7bc341f
Denver <50638006+Denver-userbot@users.noreply.github.com>
2020-06-01 14:07:43 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,40 +12,18 @@ def findat(msg):
 
 @bot.message_handler(commands=['start']) # welcome message handler
 def send_welcome(message):
-    bot.reply_to(message, '(placeholder text)')
+    bot.reply_to(message, 'Please type /register to add your account in our database!')
 
-@bot.message_handler(commands=['help']) # help message handler
-def send_welcome(message):
-    bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
-    
-@run_async
-def snipe(bot: Bot, update: Update, args: List[str]):
-    try:
-        chat_id = str(args[0])
-        del args[0]
-    except TypeError as excp:
-        update.effective_message.reply_text(
-            "Please give me a chat to echo to!")
-    to_send = " ".join(args)
-    if len(to_send) >= 2:
-        try:
-            bot.sendMessage(int(chat_id), str(to_send))
-        except TelegramError:
-            LOGGER.warning("Couldn't send to group %s", str(chat_id))
-            update.effective_message.reply_text(
-                "Couldn't send the message. Perhaps I'm not part of that group?"
-            )
-
-@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
+@bot.message_handler(func=lambda msg: msg.text is not None and '/' in msg.text)
 # lambda function finds messages with the '@' sign in them
 # in case msg.text doesn't exist, the handler doesn't process it
 def at_converter(message):
     texts = message.text.split()
     at_text = findat(texts)
-    if at_text == '@': # in case it's just the '@', skip
+    if at_text == '/': # in case it's just the '@', skip
         pass
     else:
-        insta_link = "https://instagram.com/{}".format(at_text[1:])
+        insta_link = "https://m.rivalregions.com/#slide/profile/{}".format(at_text[1:])
         bot.reply_to(message, insta_link)
 
 while True:
