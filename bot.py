9907040b2eacb953ae84dae06623f5af6a7bdc25
Denver <50638006+Denver-userbot@users.noreply.github.com>
2020-06-29 02:03:34 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ def izdvojiID(tekst):
 @bot.message_handler(commands=['roll'])
 def start(message):
     if message.chat.type == "private":
-        bot.reply_to(message, 'DICE= '🎲'')
+        bot.sendMessage(chat_id,random.randint(1,6),reply_to_message_id=msg['message_id'])
 
 @bot.message_handler(commands=['add']) # register message handler
 def send_welcome(message):
