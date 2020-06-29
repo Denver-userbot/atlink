6e3bd2a909ade50014a5fb698aa924bc4a9a81a2
Denver <50638006+Denver-userbot@users.noreply.github.com>
2020-06-29 02:20:30 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,31 +4,18 @@ import telebot
 TOKEN = "1277928698:AAGTxjacvBoQ33ouKEiTJoVv9F8b454GJyM"
 bot = telebot.TeleBot(token=TOKEN)
 
-def izdvojiID(tekst):
-    id = ''
-    for i in tekst:
-        if i.isnumeric():
-            id += i
-    return id
+def findat(msg):
+    # from a list of texts, it finds the one with the '@' sign
+    for i in msg:
+        if '@' in i:
+            return i
 
-
-
-@bot.message_handler(commands=['roll'])
-def start(message):
-    if message.chat.type == "private":
-        bot.reply_to(message, random.randint(1,6),reply_to_message_id=msg['message_id'])
-
-@bot.message_handler(commands=['add']) # register message handler
+@bot.message_handler(commands=['start']) # welcome message handler
 def send_welcome(message):
-    if message.chat.type == "private":
-        if 'rivalregions.com/#slide/profile' in message.text:
-            id = izdvojiID(message.text)
-            #dodajuFajl(id)
-            bot.reply_to(message, 'Link added ! Dont forget to join the Channel: @enclave_mercenaries')
-            bot.send_message(-1001425560791,id)
-        else:
-            bot.reply_to(message, 'Invalid format!')
-            
+    bot.reply_to(message, '(placeholder text)')
+
+@bot.message_handler(commands=['roll']) # help message handler
+    bot.sendMessage(chat_id,random.randint(1,6),reply_to_message_id=msg['message_id']) #Genera un numero da 1 a 6 e lo invia all'utente
 
 
 @bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
@@ -40,7 +27,7 @@ def at_converter(message):
     if at_text == '@': # in case it's just the '@', skip
         pass
     else:
-        insta_link = "https://m.rivalregions.com/#slide/profile/{}".format(at_text[1:])
+        insta_link = "https://instagram.com/{}".format(at_text[1:])
         bot.reply_to(message, insta_link)
 
 while True:
@@ -50,3 +37,4 @@ while True:
         # maybe there are others, therefore Exception
     except Exception:
         time.sleep(15)
+
