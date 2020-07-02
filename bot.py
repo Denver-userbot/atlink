6fde6038be5f63f609bf7d7c67e90d4db91e4ece
Denver <50638006+Denver-userbot@users.noreply.github.com>
2020-07-02 19:07:53 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['roll'])
 def start(message):
-        bot.reply_to(message, str(':die:'))
+        bot.reply_to(message, random.randint(1,6))
 
 
 @bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
