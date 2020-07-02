0b2bf616d7a7b00c8772d173e6a590019ec73496
Denver <50638006+Denver-userbot@users.noreply.github.com>
2020-07-02 03:50:45 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import time
 import telebot
 import random
-import TelegramObject
-
+from telegram import TelegramObject
 
 TOKEN = "1322615277:AAH-GTPUfDVKF8Z0caF2vTrexJEPRcPl4o8"
 bot = telebot.TeleBot(token=TOKEN)
