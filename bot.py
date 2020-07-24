c108a5212425be66ed3645094d9f8889f397b23c
Denver <50638006+Denver-userbot@users.noreply.github.com>
2020-07-24 14:00:19 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,45 +7,6 @@ from telegram import TelegramObject
 TOKEN = "1392244952:AAG0UbsIJ7rDM-JjLE9XvGPeOi1YBMior5k"
 bot = telebot.TeleBot(token=TOKEN)
           
-class Dice(TelegramObject):
-    """
-    This object represents an animated emoji with a random value for currently supported base
-    emoji. (The singular form of "dice" is "die". However, PTB mimics the Telegram API, which uses
-    the term "dice".)
-    Note:
-        If :attr:`emoji` is "🎯", a value of 6 currently represents a bullseye, while a value of 1
-        indicates that the dartboard was missed. However, this behaviour is undocumented and might
-        be changed by Telegram.
-        If :attr:`emoji` is "🏀", a value of 4 or 5 currently score a basket, while a value of 1 to
-        3 indicates that the basket was missed. However, this behaviour is undocumented and might
-        be changed by Telegram.
-    Attributes:
-        value (:obj:`int`): Value of the dice.
-        emoji (:obj:`str`): Emoji on which the dice throw animation is based.
-    Args:
-        value (:obj:`int`): Value of the dice. 1-6 for dice and darts, 1-5 for basketball.
-        emoji (:obj:`str`): Emoji on which the dice throw animation is based.
-    """
-    def __init__(self, value, emoji, **kwargs):
-        self.value = value
-        self.emoji = emoji
-
-    @classmethod
-    def de_json(cls, data, bot):
-        if not data:
-            return None
-
-        return cls(**data)
-
-    DICE = '🎲'
-    """:obj:`str`: '🎲'"""
-    DARTS = '🎯'
-    """:obj:`str`: '🎯'"""
-    BASKETBALL = '🏀'
-    """:obj:`str`: '🏀'"""
-    ALL_EMOJI = [DICE, DARTS, BASKETBALL]
-    """List[:obj:`str`]: List of all supported base emoji. Currently :attr:`DICE`,
-    :attr:`DARTS` and :attr:`BASKETBALL`."""
         
 @bot.message_handler(commands=['start']) # welcome message handler
 def send_welcome(message):
@@ -57,7 +18,7 @@ def start(message):
 
 @bot.message_handler(commands=['blackjack'])
 def start(message):
-        bot.reply_to(message, random.randint(A,J,Q K,2,3,4,5,6,7,8,9,10))
+        bot.reply_to(message, random.randint(1,10))
 
 
 @bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
